a577a50f13a7492eec220830fbfe6cb315d24040
ekroll <ekroll@pm.me>
2024-11-08 12:10:03 +0100

    space not mandatory

--- src/main/java/no/ntnu/protocol/ParseConstants.java
+++ src/main/java/no/ntnu/protocol/ParseConstants.java
@@ -7,8 +7,10 @@ public class ParseConstants {
     public static final String sensorIDRegex = "(?<sensorId>[1-9+]*)";
     public static final String sensorDataRegex = "(?<sensorID>[1-9+]*),\\s(?<actuatorState>\\w*);";
     public static final String actuatorIDRegex = "(?<actuatorId>[1-9+]*)";
-    public static final String actuatorDataRegex = "(?<actuatorID>[1-9+]*),\\s(?<actuatorState>\\w*);";
+    public static final String actuatorDataRegex = "(?<actuatorID>[1-9+]*),\s*(?<actuatorState>\w*);";
     public static final String dataRegex = "(?<data>.*)";
     public static final String responseDataRegex = "(?<responseData>\\w+$)";
 
 }
+
+// sensorID, sensorType, sensorUnit, sensorUnit
